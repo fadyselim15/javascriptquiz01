00e4616b165984338807e39462e741a25a6ac974
fadyselim15 <93913914+fadyselim15@users.noreply.github.com>
2023-08-06 18:54:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+// Q2
+
+if (i % 3 == 0 && i% 5 == 0){
+    console.log(`${i} is fizzbuzz`);
+    }
+else if(i%3 ==0){
+    console.log(`${i} is fizz`);
+}
+else if (i % 5 == 0){
+    console.log(`${i} is buzz`);
+
+
+}
+
+
+// Q5 
+
+var num1 = 44;
+var num2 = 6;
+var thesum = num1 + num2;
+
+if(num1 ==50){
+    console.log(true+" num1=50");
+}
+else if(num2 ==50){
+    console.log(true+" num2=50");
+}
+else if(thesum ==50){
+    console.log(true+" the sum = 50");
+}
+else{
+    console.log('none of these is true');
+}
