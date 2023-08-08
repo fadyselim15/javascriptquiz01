aff0375db9b3eea3aa8ba2f2ea5c15f1c6fd1a81
fadyselim15 <93913914+fadyselim15@users.noreply.github.com>
2023-08-08 13:24:31 +0300

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,160 @@
+// Q1
+/*var number = 2000;
+if (number%2==0) {
+    console.log(`the ${number} is even`);
+}
+else{
+    console.log(`the ${number} is odd`);
+}
+*/
+
+
+//-------------------------------------------------
+// Q4
+/*var r = 15;
+var aoc = Math.PI * Math.pow(r,2);
+var coc = 2*Math.PI * r;
+
+console.log('the area of circle = '+aoc);
+console.log('the Circumference = '+coc);
+*/
+
+
+
+// -----------------------------------------------
+
+// Q6
+
+/*
+var number = 20;
+if (number>0) {
+    console.log(`the ${number} is positive`);
+}
+else{
+    console.log(`the ${number} is negative`);
+
+}
+*/
+
+
+// -------------------------------------------------
+
+// Q7
+// var number = 16;
+/*if (number%5==0) {
+    console.log(`the ${number} is multiple of 5`);
+    
+}
+else if (number%8==0) {
+    console.log(`the ${number} is multiple of 8`);
+    
+}*/
+
+
+
+// -------------------------------------------------
+// Q8
+// var num1 = 20;
+// var num2 = 233;
+// var num3 = 30;
+// if (num1>num2 && num1>num3) {
+//     console.log("the num1 is the greatest");
+// }
+// else if (num2>num1 && num2>num3) {
+//     console.log("the num2 is the greatest");
+// }
+// else if (num3>num1 && num3>num) {
+//     console.log("the num3 is the greatest");
+// }
+
+// -----------------------------------------------
+// Q9
+// var x=0;
+// for (let i = 1; i <= 10 ; i++) {
+//     //  x = i +x;
+//      x+=i;
+    
+
+// }
+// console.log(x);
+// -----------------------------------------------
+// Q10
+
+// let n = 5;
+// for (let i = 1; i <= n; i++) {
+  
+//   for (let k = 0; k < i; k++) {
+//     console.log('*');
+//   }
+//   for (let j = 0; j < n - i; j++) {
+//     console.log(' ');
+//   }
+// console.log();
+// }
+
+// ------------------------------------------
+// Q11
+
+// var number = Number(prompt("enter the number"));
+// if (number>0) {
+//     console.log(`the ${number} is positive`);
+// }
+// else{
+//     console.log(`the ${number} is negative`);
+
+// }
+
+
+//=----------------------------------------------
+// Q12  
+// var array=[1,2,3,4,5,6,7,8,9,10]
+// for (let index = 0; index < array.length; index++) {
+//      console.log(`element-${index} : ${array[index]}`);
+    
+// }
+// 
+
+// ===================================================
+// Q13
+// console.log(1+3);
+
+// ==================================================
+// Q14
+
+// var x=1;
+// for (let i = 1; i <= 5 ; i++) {
+//     //  x = i +x;
+//     x*=i;
+    
+
+// }
+// console.log(x);
+
+
+// ----------------------------------------------------
+// Q15
+
+// var num1 = 40;
+// var num = 30;
+// var result = 0;
+// var opeartoer = prompt("please enter the operator")
+// switch (opeartoer) {
+//     case '+':
+//        result= num1+num2
+//         break;
+//     case '-':
+//        result= num1-num2
+//         break;
+//     case '*':
+//        result= num1*num2
+//         break;
+//     case '/':
+//        result= num1/num2
+//         break;
+
+//     default:
+//         break;
+// }
+// console.log(result);
+// ==========================================================
+
